90b1d207613957ee5e73d3d80d6308c3af12643f
Randy Harmon <rjh@Max.local>
2009-06-28 18:15:55 -0700

    New matcher for { :get => "/path" }.should route_to() and .should be_routable, and technical correction for route_for()
    Updated route_for helper to actually do the verification of route generation
    that it's advertising.  To do this, we also have to verify the route
    recognition.  That makes params_for() redundant (but it remains unchanged).
    Fully backward compatible, but unable to verify should_not cases.
    
    New route_to matcher does test both directions, differing only in syntax from
    the current method.  This syntax is simpler than route_for():
    
     { :method => "/path" }.should route_to( :action => "action", :controller => "controller" [, ...] )
    
    New be_routable [alias be_routeable] is meant to test negative cases, ensuring
    that an externally visible URL can't route anywhere.
    
    Includes documentation to help users recognize how to test should and should_not cases.

--- spec/spec/rails/example/shared_routing_example_group_examples.rb
+++ spec/spec/rails/example/shared_routing_example_group_examples.rb
@@ -21,24 +21,144 @@ share_as :RoutingExampleGroupSpec do
     route_for(:controller => "rspec_on_rails_specs", :action => "update", :id => "37").
       should == {:path => "/rspec_on_rails_specs/37", :method => :put}
   end
-
+  
+  it "on unexpected fail due to bad path, raise routing error, and suggest should_not be_routeable()" do
+    lambda {
+      route_for(:controller => "rspec_on_rails_specs", :action => "nonexistent", :id => "37") == 
+        {:path => "/rspec_on_rails_specs/bad_route/37", :method => :put}
+    }.should raise_error( ActionController::RoutingError, /suggest.*should_not be_routable/ )
+  end
+  it "on unexpected fail due to params mismatch, raise assertion, and suggest should_not be_routeable()" do
+    lambda {
+      route_for(:controller => "rspec_on_rails_specs", :action => "nonexistent", :id => "37") == 
+        {:path => "/rspec_on_rails_specs/37", :method => :put}
+    }.should raise_error( Test::Unit::AssertionFailedError, /suggest.*should_not be_routable/ )
+  end
+  it "on unexpected fail due to wrong HTTP method, raise the method error and suggest should_not be_routable() " do
+    lambda {
+      route_for(:controller => "rspec_on_rails_specs", :action => "update", :id => "37").
+        should == {:path => "/rspec_on_rails_specs/37", :method => :post}
+    }.should raise_error(ActionController::MethodNotAllowed) { |error| error.should_not =~ /should_not be_routable/ }
+  end
+  
   it "generate params for custom routes" do
+    # redundant, deprecated
     params_from(:get, '/custom_route').
       should == {:controller => "custom_route_spec", :action => "custom_route"}
   end
 
   it "generate params for existing routes" do
+    # redundant, deprecated
     params_from(:get, '/controller_spec/some_action').
       should == {:controller => "controller_spec", :action => "some_action"}
   end
 
   it "generate params for an existing route with a query parameter" do
+    # redundant, deprecated
     params_from(:get, '/controller_spec/some_action?param=1').
       should == {:controller => "controller_spec", :action => "some_action", :param => '1'}
   end
 
   it "generate params for an existing route with multiple query parameters" do
+    # redundant, deprecated
     params_from(:get, '/controller_spec/some_action?param1=1&param2=2').
       should == {:controller => "controller_spec", :action => "some_action", :param1 => '1', :param2 => '2' }
   end
+end
+
+share_as :BeRoutableExampleGroupSpec do
+  it "test should_not be_routable" do
+    { :put => "/rspec_on_rails_specs/bad_route/37" }.
+      should_not be_routable
+  end
+  it "test should be_routable" do
+    { :get => "/custom_route" }.
+      should be_routable
+  end
+    
+  it "failure from should, should recommend route_to()" do
+    lambda {
+      { :get => "/nonexisting_route" }.
+        should be_routable
+    }.should raise_error( /route_to\(/)
+  end
+
+  it "failure from should_not, should show actual route that was generated" do
+    lambda {
+      { :get => "/custom_route" }.
+        should_not be_routable
+    }.should raise_error( /"action"=>"custom_route", "controller"=>"custom_route_spec"/ )
+  end
+  
+  it "test spelling error: should_not be_routeable" do
+    { :put => "/rspec_on_rails_specs/bad_route/37" }.
+      should_not be_routeable
+  end
+  
+end
+
+share_as :RouteToExampleGroupSpec do
+  it "support custom routes" do
+    { :get => "/custom_route" }.
+      should route_to( :controller => "custom_route_spec", :action => "custom_route" )
+  end
+
+  it "support existing routes" do
+    { :get => "/controller_spec/some_action" }.
+      should route_to( :controller => "controller_spec", :action => "some_action" )
+  end
+
+  it "support existing routes with additional parameters" do
+    { :get => "/controller_spec/some_action?param=1" }.
+      should route_to( :controller => "controller_spec", :action => "some_action", :param => '1' )
+  end
+  
+  it "recognize routes with methods besides :get" do
+    { :put => "/rspec_on_rails_specs/37" }.
+      should route_to(:controller => "rspec_on_rails_specs", :action => "update", :id => "37")
+  end
+  
+  it "helps the user format the path properly" do
+    lambda {"a string instead of a Hash".
+      should route_to(:controller => "rspec_on_rails_specs", :action => "update", :id => "37") 
+    }.should raise_error( ArgumentError, /usage/ )
+  end
+  
+  it "allows only one key/value in the path - :method => path" do
+    lambda {
+      { :a => "b" ,:c => "d" }.
+        should route_to("anything")
+    }.should raise_error( ArgumentError, /usage/ )
+  end
+  
+  it "on unexpected fail due to bad path, raise routing error, and suggest should_not be_routeable()" do
+    lambda {
+      { :put => "/rspec_on_rails_specs/nonexistent/37" }.
+        should route_to(:controller => "rspec_on_rails_specs", :action => "nonexistent", :id => "37")
+        
+    }.should raise_error( ActionController::RoutingError, /suggest.*nonexistent.*should_not be_routable/ )
+  end
+  it "on unexpected fail due to params mismatch, raise assertion, and suggest should_not be_routeable()" do
+    lambda {
+      { :put => "/rspec_on_rails_specs/37" }.
+        should route_to(:controller => "rspec_on_rails_specs", :action => "nonexistent", :id => "37")
+    }.should raise_error( Test::Unit::AssertionFailedError, /suggest.*rspec_on_rails_specs\/37.*should_not be_routable/ )
+  end
+
+  it "when an expected failure passes instead, suggest should_not be_routable" do
+    lambda {
+      { :put => "/rspec_on_rails_specs/37" }.
+        should_not route_to(:controller => "rspec_on_rails_specs", :action => "update", :id => "37")
+    }.should raise_error( /expected a routing error.*be_routable/im )
+    
+  end
+
+  it "on unexpected fail due to wrong HTTP method, raise the method error and suggest should_not be_routable()" do
+    lambda {
+      { :post => "/rspec_on_rails_specs/37" }.
+        should route_to(:controller => "rspec_on_rails_specs", :action => "update", :id => "37" )
+    }.should raise_error(ActionController::MethodNotAllowed, /rspec_on_rails_specs\/37.*should_not be_routable/ )
+  end
+  
+  
 end
